1a97874a15c807fed2b1d2c1f9420851bba5d965
SJubuntux <SJ@ubuntux.com>
2025-11-18 20:21:28 -0600

    terraform setup and deploy files on docker

---
+++ infra/setup/variables.tf
@@ -0,0 +1,18 @@
+variable "tf_state_bucket" {
+  description = "Name of S3 bucket in AWS for storing TF state"
+  default     = "devops-recepie-app"
+}
+
+variable "tf_state_lock_table" {
+  description = "Name of the DynamoDB table for TF state locking"
+  default     = "devops-recepie-app-lock"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
+}
+variable "contact" {
+  description = "Contact name for tagging resources"
+  default     = "SJ@ubuntux.com"
+}
