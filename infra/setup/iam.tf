868ed1a90f50c48b6f00fdb4a1c96df25de4916e
SJubuntux <SJ@ubuntux.com>
2025-11-28 14:41:37 -0600

    Fix Adding ECS permission for manage service-linked roles

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -186,6 +186,28 @@ resource "aws_iam_user_policy_attachment" "rds" {
 #########################
 # Policy for ECS access #
 #########################
+# Custom ECS Task Execution Role (for ECS tasks)
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "CustomECSTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 data "aws_iam_policy_document" "ecs" {
   statement {
